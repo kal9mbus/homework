8d774312cc6e8105a428ff563d908e4dc971a4da
NikolayLaptop <nik.shevchuk.adm@list.ru>
2021-05-17 22:16:05 +1100

    final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict"
 
-let money = prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD","");
+//let money = 20000, time = 2021-05-15, question1 = "JKX", question2 = 1500, question3 = "SVET", question4 = 2500;
+let money = prompt("Ваш бюджет на месяц?", ""), time = prompt("Введите дату в формате YYYY-MM-DD","");
 let question1 = prompt("Введите обязательную статью расходов в этом месяце",""),  question2 = prompt("Во сколько обойдется?",""), question3 = prompt("Введите обязательную статью расходов в этом месяце", ""), question4 = prompt ("Во сколько обойдется?","");
 
 
@@ -14,5 +14,6 @@ let appData = {
     savings: false
 };
 appData.expenses.question1 = question2;
+appData.expenses.question3 = question4;
 alert("Бюджет на 1 день : "+appData.budget/30);
-alert(appData.expenses.question1);
+console.log(appData)
