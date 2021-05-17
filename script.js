4246ac774c9ef44c9d280b8e0f166f5a3bb3a3ed
Nikolay <nik.shevchuk.adm@list.ru>
2021-05-17 17:44:50 +1100

    homework

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
+'use strict'
 
-alert("all fine");
+let money = prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD","");
+let question1 = prompt("Введите обязательную статью расходов в этом месяце","");
+let question2 = prompt("Во сколько обойдется?","");
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+appData.expenses.question1 = question2;
+alert("Бюджет на 1 день : "+appData.budget/30);
+alert(appData.expenses.question1);
